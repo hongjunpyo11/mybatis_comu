5d79965ce9f206c75aff667c42ae175b26556491
hongjunpyo11 <jpyo98@naver.com>
2023-02-05 18:26:02 +0900

    Ut.empty 함수 도입

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.exam.demo.service.MemberService;
+import com.sbs.exam.demo.util.Ut;
 import com.sbs.exam.demo.vo.Member;
 
 @Controller
@@ -19,27 +20,27 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object getString(String loginId, String loginPw, String name, 
 							String nickname, String cellphoneNo, String email) {
-		if (loginId == null) {
+		if (Ut.empty(loginId)) {
 			return "loginId(을)를 입력해주세요.";
 		}
 
-		if (loginPw == null) {
+		if (Ut.empty(loginPw)) {
 			return "loginPw(을)를 입력해주세요.";
 		}
 
-		if (name == null) {
+		if (Ut.empty(name)) {
 			return "name(을)를 입력해주세요.";
 		}
 
-		if (nickname == null) {
+		if (Ut.empty(nickname)) {
 			return "nickname(을)를 입력해주세요.";
 		}
 
-		if (cellphoneNo == null) {
+		if (Ut.empty(cellphoneNo)) {
 			return "cellphoneNo(을)를 입력해주세요.";
 		}
 
-		if (email == null) {
+		if (Ut.empty(email)) {
 			return "email(을)를 입력해주세요.";
 		}
 		
