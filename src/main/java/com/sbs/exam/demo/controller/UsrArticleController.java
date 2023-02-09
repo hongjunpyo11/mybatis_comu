260c6c19847db75d3819f0b2d980913a3bb6503d
hongjunpyo11 <jpyo98@naver.com>
2023-02-09 22:41:55 +0900

    게시물 상세페이지가 보여질 때 마다, 조회수 1 증가

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -56,6 +56,13 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(Model model, int id) {
+		
+		ResultData<Integer> increaseHitCountRd = articleService.increaseHitCount(id);
+		
+		if ( increaseHitCountRd.isFail() ) {
+			return rq.historyBackJsOnView(increaseHitCountRd.getMsg());
+		}
+		
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		model.addAttribute("article", article);
@@ -158,29 +165,3 @@ public class UsrArticleController {
 		return rq.jsReplace(Ut.f("%d번 글이 생성되었습니다.", id), replaceUri);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
